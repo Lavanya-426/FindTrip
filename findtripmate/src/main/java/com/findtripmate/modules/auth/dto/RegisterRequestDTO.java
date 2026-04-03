6c149e15315f44d73bae306951a923ed2543255a
Lavanya <lavanyaanna246@gmail.com>
2026-04-03 21:00:37 +0530

    added email validation regex

--- findtripmate/src/main/java/com/findtripmate/modules/auth/dto/RegisterRequestDTO.java
+++ findtripmate/src/main/java/com/findtripmate/modules/auth/dto/RegisterRequestDTO.java
@@ -2,6 +2,7 @@ package com.findtripmate.modules.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -10,8 +11,12 @@ public class RegisterRequestDTO {
     @NotBlank
     private String name;
 
-    @Email
     @NotBlank
+    @Email
+    @Pattern(
+        regexp = "^[A-Za-z0-9._%+-]+@vitapstudent\\.ac\\.in$",
+        message = "Email must be a VIT-AP student email"
+    )
     private String email;
 
     @NotBlank
